d0555c52bb3b37c45f6b560d0e55ed6f30bfbe81
SanketUdapi <72694382+SanketUdapi@users.noreply.github.com>
2024-11-21 12:10:08 +0000

    added expiration and indexing

--- src/models/Coupon.js
+++ src/models/Coupon.js
@@ -3,8 +3,18 @@ const mongoose = require('mongoose');
 const CouponSchema = new mongoose.Schema({
   type: { type: String, enum: ['cart-wise', 'product-wise', 'bxgy'], required: true },
   details: { type: Object, required: true }, 
-  expirationDate: { type: Date },
+  expirationDate: { type: Date, default: null },
   isActive: { type: Boolean, default: true },
 });
+
+// ttl index to automaticlly delete document after expiration date
+CouponSchema.index(
+  { expirationDate: 1 },
+  { expireAfterSeconds: 0, partialFilterExpression: { expirationDate: { $exists: true, $ne: null } } }
+);
+
+// added index to optimize quering
+CouponSchema.index({ type: 1 });
+CouponSchema.index({ isActive: 1 });
 
 module.exports = mongoose.model('Coupon', CouponSchema);
